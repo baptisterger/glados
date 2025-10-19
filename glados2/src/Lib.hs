8673e26a79b420f0397c5cbf7f03ab979720164a
Baptiste <baptiste.roger@epitech.eu>
2025-10-19 22:40:33 +0200

    Add: right parsing functions

--- glados2/src/Lib.hs
+++ glados2/src/Lib.hs
@@ -50,3 +50,77 @@ parseFloat = do
   return $ FloatLit (case sign of
     Just _ -> -num
     Nothing -> num)
+
+parseWord :: Parser Token
+parseWord = do
+  word <- many1 (alphaNum <|> char '_')
+  notFollowedBy (alphaNum <|> char '_')
+  return $ case word of
+    "true" -> BoolLit True
+    "false" -> BoolLit False
+    "skibidi" -> Keyword word
+    "main" -> Keyword word
+    "float" -> Keyword word
+    "int" -> Keyword word
+    "bool" -> Keyword word
+    "if" -> Keyword word
+    "else" -> Keyword word
+    "print" -> Keyword word
+    _ -> Identifier word
+
+parseOperator :: Parser Token
+parseOperator = do
+  op <- choice
+    [ try (string "==")
+    , try (string "!=")
+    , try (string "<=")
+    , try (string ">=")
+    , string "+"
+    , string "-"
+    , string "*"
+    , string "/"
+    , string "="
+    , string "<"
+    , string ">"
+    ]
+  return $ Operator op
+
+parseDelimiter :: Parser Token
+parseDelimiter = do
+  delim <- oneOf "(){}[];,"
+  return $ Delimiter delim
+
+parseToken :: Parser Token
+parseToken = 
+  try parseFloat <|>
+  try parseInt <|>
+  try parseWord <|>
+  try parseOperator <|>
+  parseDelimiter
+
+parseTokens :: Parser [Token]
+parseTokens = do
+  skipSpaces
+  tokens <- sepEndBy parseToken skipSpaces
+  eof
+  return tokens
+
+parseFile :: String -> IO ()
+parseFile filename = do
+  content <- readFile filename
+  case parse parseTokens filename content of
+    Left err -> print err
+    Right tokens -> mapM_ print tokens
+
+someFunc :: IO ()
+someFunc = do
+  args <- getArgs
+  case args of
+    [filename] -> parseFile filename
+    [] -> do
+      putStrLn "Reading from stdin..."
+      content <- getContents
+      case parse parseTokens "stdin" content of
+        Left err -> print err
+        Right tokens -> mapM_ print tokens
+    _ -> putStrLn "Usage: program [filename] or use redirection"
