a84e32fb142c097cf34b7fca3f87ac6192e67a9b
ArnaudEPI <146713528+ArnaudEPI@users.noreply.github.com>
2025-11-03 15:35:54 +0100

    Update vm.py

--- bonus/vm.py
+++ bonus/vm.py
@@ -1,45 +1,46 @@
 import sys
-from wasmtime import Store, Module, Instance, Func, FuncType, ValType, ExportType
-
-def host_print_i32(arg: int):
-    print(arg)
-
-def host_print_f32(arg: float):
-    print(arg)
+from wasmtime import Store, Module, Instance, Func, FuncType, ValType, Linker
 
 class WasmRunner:
     def __init__(self, wasm_or_wat_path):
         self.store = Store()
         self.module = Module.from_file(self.store.engine, wasm_or_wat_path)
+        self.linker = Linker(self.store.engine)
+
+
+        def host_print_i32(arg: int):
+            print(arg)
+
+        def host_print_f32(arg: float):
+            print(arg)
+
+        def host_print_string(address: int, length: int):
+            memory = self.instance.exports(self.store).get("memory")
+            if memory is None:
+                raise TypeError("WebAssembly module must export a memory to print strings.")
+            
+            data = memory.read(self.store, address, length)
+            
+            print(data.decode('utf-8'))
+
+        # --- Linking ---
 
         print_i32_type = FuncType([ValType.i32()], [])
-        self.host_print_i32_func = Func(self.store, print_i32_type, host_print_i32)
-
         print_f32_type = FuncType([ValType.f32()], [])
-        self.host_print_f32_func = Func(self.store, print_f32_type, host_print_f32)
+        print_string_type = FuncType([ValType.i32(), ValType.i32()], [])
 
-        imports = []
-        for import_type in self.module.imports:
-            if import_type.module == "env" and import_type.name == "print":
-                if isinstance(import_type.type, FuncType):
-                    if import_type.type.params == [ValType.f32()]:
-                        imports.append(self.host_print_f32_func)
-                    elif import_type.type.params == [ValType.i32()]:
-                        imports.append(self.host_print_i32_func)
-                    else:
-                        raise Exception(f"Unsupported print function type: {import_type.type.params}")
-                else:
-                    raise Exception(f"Unsupported import type: {import_type.type}")
-            else:
-                raise Exception(f"Unknown import: {import_type.module}.{import_type.name}")
-
-        self.instance = Instance(self.store, self.module, imports)
+        self.linker.define_func("env", "print_i32", print_i32_type, host_print_i32)
+        self.linker.define_func("env", "print_f32", print_f32_type, host_print_f32)
+        self.linker.define_func("env", "print_str", print_string_type, host_print_string)
+        
+        self.instance = self.linker.instantiate(self.store, self.module)
 
     def run(self):
-        main_func = self.instance.exports(self.store)["main"]
-        if main_func:
+        main_func = self.instance.exports(self.store).get("main")
+        if main_func is not None:
             result = main_func(self.store)
-            print(f"main() returned: {result}")
+            if result is not None:
+                print(f"main() returned: {result}")
         else:
             print("Error: 'main' function not found in WASM module.")
 
